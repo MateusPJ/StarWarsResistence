ccb58a2ef83f31c0cd4d69a7582d0c1ca7385a0c
luiz <simoes.segundo@gmail.com>
2019-05-27 08:49:19 -0300

    Metodoq ue retornar os rebeldes não traidores

--- StarWarsResistence/src/main/java/br/com/phoebus/star/repositorios/IRebeldeRepositorio.java
+++ StarWarsResistence/src/main/java/br/com/phoebus/star/repositorios/IRebeldeRepositorio.java
@@ -3,6 +3,10 @@
  */
 package br.com.phoebus.star.repositorios;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+
 import br.com.phoebus.star.entidades.Rebelde;
 
 /**
@@ -11,4 +15,21 @@ import br.com.phoebus.star.entidades.Rebelde;
  */
 public interface IRebeldeRepositorio extends RepositorioBase<Rebelde, Long> {
 
+	/**
+	 * 
+	 * Retorna todos os rebeldes qeu NÃO são traidores. 
+	 * 
+	 * 	@author Mateus P Jorge
+	 */
+	@Query(
+	  value = "select * " + 
+	  		"from tb_rebelde reb " + 
+	  		"where reb.id not in (select traidores.id_traidor " + 
+	  		"		    from (select r.id_traidor, count(r.id_traidor) " + 
+	  		"			  from tb_report r " + 
+	  		"			  group by r.id_traidor " + 
+	  		"			  HAVING count(r.id_traidor) >= 3) traidores)", 
+	  nativeQuery = true)
+	@Override
+	List<Rebelde> findAll();
 }
